4b9b89c65397d10a671d208b7ba072e9b4f102b2
Ibrahim Olayinka <ibrahimolayinkaa@gmail.com>
2019-06-13 18:51:44 +0100

    another commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <add-blog/>
+  </div>
+</template>
+
+<script>
+import AddBlog from './components/AddBlog.vue'
+
+export default {
+  name: 'app',
+  components: {
+          'add-blog':AddBlog
+  },
+  data(){
+      return {
+
+      }
+  },
+  method: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/AddBlog.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h3>Hello James</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'AddBlog',
+        data(){
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style  scoped>
+
+</style>
