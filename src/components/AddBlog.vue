033d9efa42b6dabd878e0b24aed618d5660988f6
Ibrahim Olayinka <ibrahimolayinkaa@gmail.com>
2019-06-13 19:12:45 +0100

    another commit

--- src/components/AddBlog.vue
+++ src/components/AddBlog.vue
@@ -1,6 +1,23 @@
 <template>
-    <div>
-        <h3>Hello James</h3>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 offset-md-2">
+                <h3>Add A New Blog Post</h3>
+                <form>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Title</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Post</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                  
+                    <button type="submit" class="btn btn-primary btn-block">Submit</button>
+                </form>
+            </div>
+        </div>
+      
     </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import jQuery from 'jquery'
+import bootstrap from 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+global.jQuery = jQuery
+global .$ = jQuery
 
 Vue.config.productionTip = false
 
